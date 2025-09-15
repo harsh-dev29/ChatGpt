ae41a63435d4b9b69abd26945b0dfa611ce72812
harsh vishnoi <harshvishnoioffcial@gmail.com>
2025-09-15 14:26:38 +0530

    third commit

--- backend/src/app.js
+++ backend/src/app.js
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, '../public')))
 app.use('/api/auth', authRoutes)
 app.use('/api/chat', chatRoutes)
 
-app.get('*', () => {
+app.get('*name', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'))
 })
 
